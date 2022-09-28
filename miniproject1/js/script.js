0453f190f54e7a91da322458c86e69133cff50e9
fbgabriana <fbgabriana@gmail.com>
2022-09-28 21:17:32 +0800

    Random changes

--- miniproject1/js/script.js
+++ miniproject1/js/script.js
@@ -13,7 +13,7 @@ function toggleMenuBar() {
 function windowResize() {
 	var menubar = document.getElementById("menubar");
 	var menubtn = document.getElementById("menubtn");
-	var heading = document.getElementsByTagName("h1")[0];
+	var heading = document.querySelector("h1");
 	if (window.innerWidth <= 500) {
 		heading.style.fontSize = window.innerWidth / 192 + "rem";
 		menubar.style.display = "none";
@@ -30,17 +30,17 @@ function windowResize() {
 }
 
 function setDocumentTitle() {
-	var heading = document.getElementsByTagName("article")[0].getElementsByTagName("h2")[0];
+	var heading = document.querySelector("article").querySelector("h2");
 	document.title = "Philippine Astronomy: " + heading.innerHTML;
 }
 
 function setCurrentPage() {
-	var a = document.getElementsByTagName("a");
+	var a = document.querySelectorAll("a");
 	var currentLocation = location.href.replace("index.html","");
 	for (i=0; i<a.length; i++) {
 		if (a[i].href == currentLocation) {
 			a[i].classList.add("current-page");
-			a[i].href = "javascript:void(0)";
+			a[i].removeAttribute("href");
 		}
 		if (currentLocation.includes(a[i].href)) {
 			a[i].classList.add("current-section");
@@ -58,4 +58,3 @@ window.onload = function() {
 window.onresize = function() {
 	windowResize();
 }
-
