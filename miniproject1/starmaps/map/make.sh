8f2ca3d46a95fe767618e1ac5d41c346fa366f08
fbgabriana <fbgabriana@gmail.com>
2022-09-28 20:58:42 +0800

    starmap color changes

--- miniproject1/starmaps/map/make.sh
+++ miniproject1/starmaps/map/make.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 papersize=Letter
 
-rm -f *.pdf *.ps *.svg *.png
+rm -f *.pdf *.svg
 for RR in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23; do
-starmap -D 15 -R $RR -F 180 -M 5 +c +e -b -g -d +t -m -G 1024x1024 -s skyglobe -o $RR.svg
-starmap -D 15 -R $RR -F 180 -M 5 +c +e +b -g -d +t -m -G 1024x1024 -s skyglobe -o $RR+b.svg
-starmap -D 15 -R $RR -F 180 -M 5 +c +e -b +g -d +t -m -G 1024x1024 -s skyglobe -o $RR+g.svg
-starmap -D 15 -R $RR -F 180 -M 5 +c +e +b +g -d +t -m -G 1024x1024 -s skyglobe -o $RR+bg.svg
+starmap -D 15 -R $RR -F 180 -M 5 +c +e -b -g -d +t -m -G 1024x1024 -s screen -o $RR.svg
+starmap -D 15 -R $RR -F 180 -M 5 +c +e +b -g -d +t -m -G 1024x1024 -s screen -o $RR+b.svg
+starmap -D 15 -R $RR -F 180 -M 5 +c +e -b +g -d +t -m -G 1024x1024 -s screen -o $RR+g.svg
+starmap -D 15 -R $RR -F 180 -M 5 +c +e +b +g -d +t -m -G 1024x1024 -s screen -o $RR+bg.svg
 sed -i 's/fill:rgb(0%,0%,0%);fill-opacity:1/fill:rgb(0%,0%,0%);fill-opacity:0/g' $RR.svg $RR+b.svg $RR+g.svg $RR+bg.svg
 starmap -D 15 -R $RR -F 180 -M 5 +c +e -b -g -d +t -m -P 400x300 -s print -o $RR.pdf
 starmap -D 15 -R $RR -F 180 -M 5 +c +e +b -g -d +t -m -P 400x300 -s print -o $RR+b.pdf
@@ -18,4 +18,3 @@ gs $gssettings -dDEVICEWIDTHPOINTS=1080 -dDEVICEHEIGHTPOINTS=864 -dFIXEDMEDIA -d
 gs $gssettings -sPAPERSIZE=$(echo "$papersize" | tr '[:upper:]' '[:lower:]')  -dPDFFitPage -sDEVICE=pdfwrite -sOutputFile="$RR.pdf" "$tmpfile"
 rm "$tmpfile"
 done
-stamp
