9f82be320874bebdd703dbb64579defaf85447ee
fbgabriana <fbgabriana@gmail.com>
2022-10-25 04:23:20 +0800

    Random updates

--- miniproject2/scripts/scripts.js
+++ miniproject2/scripts/scripts.js
@@ -5,16 +5,22 @@ let backgroundTransition = () => {
 			img[i] = new Image();
 			img[i].src = `images/background${i}.jpg`;
 		}
+		let benefits = ["marketability online", "growth opportunities", "competitiveness", "market expansion", "consumer services", "your credibility"];
 		setTimeout(() => {
 			let i = 0;
 			let j = 0;
 			setInterval(() => {
 				j = i; i = (i + 1) % 6;
 				replaceBackground(i, j);
+				replaceText(i);
 			}, 4000);
 
 			let replaceBackground = (i, j) => {
 				document.body.style.backgroundImage = `url(images/background${i}.jpg), url(images/background${j}.jpg)`;
+			}
+			let replaceText = i => {
+				document.querySelector(".boost").innerHTML = benefits[i];
+				console.log(benefits[i]);
 			}
 		}, 1000);
 	})
