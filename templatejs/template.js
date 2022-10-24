bb0df913541229f1424698c575ace3761fc3581e
fbgabriana <fbgabriana@gmail.com>
2022-10-24 08:00:49 +0800

    Random updates

---
+++ templatejs/template.js
@@ -0,0 +1,25 @@
+class Template {
+	constructor(path) {
+		this.path = path;
+		this.selector = "";
+	}
+	loadContent() {
+		fetch(this.path).then(response => {
+			return response.text();
+		}).then(string => {
+			let parser = new DOMParser();
+			this.document = parser.parseFromString(string, "text/html");
+			this.document.querySelector(this.selector).replaceWith(document.querySelector(this.selector));
+			this.document.title = `${this.document.title} — ${document.title}`;
+			document.documentElement.replaceWith(this.document.documentElement);
+		}).catch(error => {  
+			console.log(error);  
+		});
+	}
+}
+
+window.addEventListener("DOMContentLoaded", event => {
+	let template = new Template("template.html");
+	template.selector = "#content";
+	template.loadContent();
+});
