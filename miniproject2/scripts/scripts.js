6a70ca0b180ef96cd886bcdb03f6e3c85a7e03e2
fbgabriana <fbgabriana@gmail.com>
2022-10-24 20:32:46 +0800

    Random updates

--- miniproject2/scripts/scripts.js
+++ miniproject2/scripts/scripts.js
@@ -1,12 +1,20 @@
 let backgroundTransition = () => {
-	let i = 0;
-	let j = 0;
-	setInterval(() => {
-		j = i; i = (i + 1) % 6;
-		replaceBackground(i, j);
-	}, 3000);
-	let replaceBackground = (i, j) => {
-		document.body.style.backgroundImage = `url(images/background${i}.jpg), url(images/background${j}.jpg)`;
+	let img = [];
+	for (let i = 0; i < 6; i++) {
+		img[i] = new Image();
+		img[i].src = `images/background${j}.jpg`;
 	}
+	setTimeout(() => {
+		let i = 0;
+		let j = 0;
+		setInterval(() => {
+			j = i; i = (i + 1) % 6;
+			replaceBackground(i, j);
+		}, 4000);
+
+		let replaceBackground = (i, j) => {
+			document.body.style.backgroundImage = `url(images/background${i}.jpg), url(images/background${j}.jpg)`;
+		}
+	}, 1000);
 }
 
