07cb4f5082f90fd92a19fcdb95d55dd60b06166b
fbgabriana <fbgabriana@gmail.com>
2022-10-25 15:40:37 +0800

    Random updates

--- miniproject2/scripts/scripts.js
+++ miniproject2/scripts/scripts.js
@@ -1,5 +1,6 @@
-let backgroundTransition = () => {
-	window.addEventListener("load", (event) => {
+let setBackground = () => {
+	if (location.href.includes("home.html") || location.href.includes("template.html")) {
+		document.body.style.backgroundImage = `url(images/background0.jpg), url(images/background.jpg)`;
 		let img = [];
 		for (let i = 0; i < 6; i++) {
 			img[i] = new Image();
@@ -20,8 +21,37 @@ let backgroundTransition = () => {
 			}
 			let replaceText = i => {
 				document.querySelector(".boost").innerHTML = benefits[i];
-				console.log(benefits[i]);
 			}
 		}, 1000);
-	})
+	}
+	if (location.href.includes("about.html")) {
+		document.body.style.backgroundImage = `url(images/background1.jpg), url(images/background.jpg)`;
+	}
+	if (location.href.includes("projects.html")) {
+		document.body.style.backgroundImage = `url(images/background3.jpg), url(images/background.jpg)`;
+	}
+	if (location.href.includes("contact.html")) {
+		document.body.style.backgroundImage = `url(images/background4.jpg), url(images/background.jpg)`;
+	}
 }
+
+let setCurrentPage = () => {
+	var a = document.querySelectorAll("a");
+	var currentLocation = location.href.replace("index.html","");
+	for (i=0; i<a.length; i++) {
+		if (a[i].href == currentLocation) {
+			a[i].classList.add("current-page");
+			a[i].removeAttribute("href");
+			a[i].removeAttribute("title");
+		}
+		if (currentLocation.includes(a[i].href)) {
+			a[i].classList.add("current-section");
+		}
+	}
+}
+
+window.addEventListener("load", event => {
+	setCurrentPage();
+	setBackground();
+});
+
