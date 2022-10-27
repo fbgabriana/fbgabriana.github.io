cc948eb14ec15babeeced74970410a1d836b34c1
fbgabriana <fbgabriana@gmail.com>
2022-10-27 15:45:01 +0800

    Random updates

--- miniproject2/scripts/scripts.js
+++ miniproject2/scripts/scripts.js
@@ -1,5 +1,33 @@
 let setBackground = () => {
 
+	const d0 = new Date();
+
+	const getFrameNumber = () => {
+		let d1 = new Date();
+		return Math.floor( ( d1.getTime() - d0.getTime() ) / 4000 ) % 6;
+	}
+
+	const getColorScheme = () => {
+		return (window.matchMedia("(prefers-color-scheme: dark)").matches) ? "-dark" : "";
+	}
+
+	const changeFrame = () => {
+		f = getFrameNumber();
+		s = getColorScheme();
+		// console.log(f, s);
+	}
+
+	const currentColorScheme = "";
+	setInterval(checkSchemeChange = () => {
+		if (currentColorScheme != getColorScheme()) {
+			console.log(`scheme changed to ${getColorScheme()}`);
+		}
+	}, 100);
+
+	setInterval(rotateFrames = () => {
+		changeFrame();
+	}, 4000);
+
 	let preloadImages = () => {
 		let img1 = []; let img2 = [];
 		for (let i = 0; i < 6; i++) {
@@ -10,33 +38,29 @@ let setBackground = () => {
 	}
 	preloadImages();
 
-	let getColorScheme = () => {
-		return (window.matchMedia("(prefers-color-scheme: dark)").matches) ? "-dark" : ""
-	};
-
 	if (location.href.includes("home.html")) {
 		let s = getColorScheme();
 		document.body.style.backgroundImage = `url(images/background0${s}.jpg), url(images/background${s}.jpg)`;
 
 		let benefits = ["marketability online", "growth opportunities", "competitiveness", "market expansion", "consumer services", "your credibility"];
 
+		let replaceBackground = (i, j) => {
+			let s = getColorScheme();
+			document.body.style.backgroundImage = `url(images/background${i}${s}.jpg), url(images/background${j}${s}.jpg)`;
+		}
+		let replaceText = i => {
+			let boost = document.querySelector(".boost");
+			if (boost) document.querySelector(".boost").childNodes[0].nodeValue = benefits[i];
+		}
+
 		setTimeout(waitForPreload = () => {
 			let i = 0;
 			let j = 0;
-			setInterval(() => {
+			setInterval(setNewFrame = () => {
 				j = i; i = (i + 1) % 6;
 				replaceBackground(i, j);
 				replaceText(i);
 			}, 4000);
-
-			let replaceBackground = (i, j) => {
-				let s = getColorScheme();
-				document.body.style.backgroundImage = `url(images/background${i}${s}.jpg), url(images/background${j}${s}.jpg)`;
-			}
-			let replaceText = i => {
-				let boost = document.querySelector(".boost");
-				if (boost) document.querySelector(".boost").childNodes[0].nodeValue = benefits[i];
-			}
 		}, 1000);
 	}
 
