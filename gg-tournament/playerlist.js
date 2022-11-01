6de0d8d7c3ddb227709ff19539ef69b93fe58d9d
fbgabriana <fbgabriana@gmail.com>
2022-11-01 09:36:35 +0800

    added gg-tournament

---
+++ gg-tournament/playerlist.js
@@ -0,0 +1,34 @@
+var playerlist =
+
+// The following data is in Javascript Object Notation format.
+// They are given as name-value pairs separated by a colon.
+// Each pair is separated by a comma.
+
+// To edit, only change what is inside the quotation marks
+// in the 2nd column. Do not remove the braces, quotes,
+// colons, and commas.
+
+{
+
+"R1": "RedTeam1" ,
+"R2": "RedTeam2" ,
+"R3": "RedTeam3" ,
+"R4": "RedTeam4" ,
+
+"B1": "BlueTeam1" ,
+"B2": "BlueTeam2" ,
+"B3": "BlueTeam3" ,
+"B4": "BlueTeam4" ,
+
+"G1": "GoldTeam1" ,
+"G2": "GoldTeam2" ,
+"G3": "GoldTeam3" ,
+"G4": "GoldTeam4" ,
+
+"K1": "BlackTeam1" ,
+"K2": "BlackTeam2" ,
+"K3": "BlackTeam3" ,
+"K4": "BlackTeam4" ,
+
+}
+
