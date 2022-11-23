84ffda6f6b9e10ada78741fe342e786f4e2673c2
fbgabriana <fbgabriana@gmail.com>
2022-11-23 19:01:10 +0800

    Random updates

--- philippine-astronomy/js/script.js
+++ philippine-astronomy/js/script.js
@@ -96,7 +96,7 @@ const addColorSwitcher = (selector) => {
 
 		checkbox_colorswitcher.addEventListener("change", event => {
 			set_colorscheme();
-			if (navigator.vibrate) navigator.vibrate(200);
+			if (navigator.vibrate) navigator.vibrate(50);
 		});
 	}
 }
